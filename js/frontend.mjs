9da5a05c6189330a0e872f53e80437caa129f799
Marian Orszagh <morszagh@redhat.com>
2019-11-21 17:47:02 +0200

    Implement front end and map functionality

---
+++ js/frontend.mjs
@@ -0,0 +1,130 @@
+const SIDEBAR_ID = '#sidebar';
+const NOTE_CLASS = 'note-card'
+// TEMPORARY test data
+var NOTES = [
+]
+
+/**
+ * Find note by its ID.
+ * @param notes Array of notes objects.
+ * @param noteId The ID of the note to be found.
+ * @returns The index of the note in the notes array.
+ */
+var findNote = function (notes, noteId) {
+    let id;
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id_ == noteId) {
+            id = i;
+        }
+    }
+    return id;
+}
+
+/**
+ * Update a note's information in the database.
+ * @param {*} noteId The note's id in the db.
+ * @param {*} text Text of the note to be updated.
+ */
+var updateNote = function (notes, noteId, text) {
+    console.log('Updating note ' + noteId);
+    let id = findNote(notes, noteId);
+    notes[id].text = text;
+    // TODO: Update database;
+    // dbUpdateNote(id, text)
+}
+
+
+/**
+ * Resize a note based on the amount of lines/text
+ * @param {*} element The textarea element to be resized
+ * https://stackoverflow.com/questions/2803880/is-there-a-way-to-get-a-textarea-to-stretch-to-fit-its-content-without-using-php
+ */
+var resizeNote = function (element) {
+    element.style.height = "";
+    element.style.height = element.scrollHeight + 3 + "px";
+}
+
+/**
+ * Removes a note from the DOM tree and sends a delete request to the DB.
+ * @param {*} notes List of notes objects.
+ * @param {*} noteId Note's ID.
+ */
+var removeNote = function (notes, noteId) {
+    // Collapse the elements, then delete them, after a small wait, for smoother animation
+    note = document.getElementById(noteId);
+    for (c of note.querySelectorAll('h5, button, textarea')) {
+        c.classList.add('collapsed');
+    }
+    note.classList.add('collapsed');
+    setTimeout(function () { document.getElementById(noteId).remove() }, 400);
+    // TODO: Remove from DB
+    // dbRemoveNote(noteId)
+    // Remove from local registry
+    notes.splice(findNote(notes, noteId), 1);
+    // Remove the marker from the map
+    MARKERS[noteId].setMap(null);
+}
+
+/**
+ * Sets the focus on a given note.
+ * @param id Id of the note.
+ */
+var focusNote = function (id) {
+    // Open the sidebar, if it's not open
+    $('#main').addClass('active');
+    $('#map').addClass('active');
+    // Set focus on the note
+    $(`#${id} textarea`).focus()
+}
+
+/**
+ * Centers the map on the note's marker.
+ * @param {*} location Location on which the map should be centered.
+ * @param {*} map The map object.
+ */
+var centerNote = function (lat, lng, map) {
+    console.log('Centering map to ' + lat + ' ' + lng);
+    map.map.setCenter({ 'lat': lat, 'lng': lng });
+}
+
+/**
+ * Deletes and redraws all notes in the DOM tree.
+ * @param notes List of notes.
+ */
+var redrawNotes = function (notes) {
+    document.getElementById('sidebar').innerHTML = "";
+    drawNotes(notes);
+}
+
+/**
+ * Draws notes onto the sidebar
+ * @param {*} notes Array of note objects.
+ */
+var drawNotes = function (notes) {
+    for (let n of notes) {
+        $(SIDEBAR_ID).append(
+            `
+            <div class="${NOTE_CLASS}" id="${n.id_}">
+                <button type="button" class="close" aria-label="Close"
+                    onclick='removeNote(NOTES, this.parentElement.id)'>
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h5 onclick='centerNote(${n.location.lat}, ${n.location.lng}, MAP)'>${n.locationTitle}</h5>
+                <textarea 
+                    oninput='resizeNote(this)'
+                    onresize='resizeNote(this)'
+                    onfocusout='updateNote(NOTES, this.parentElement.id, this.value);'
+                    placeholder="Enter a note...">${n.text}</textarea>
+            </div>
+            `
+        )
+    }
+    // Change initial textarea size, a slight wait is necessary for the page to load
+    // correctly the first time
+    setTimeout(() => {
+        $("textarea").each(function () {
+            resizeNote(this);
+        });
+    }, 100)
+
+}
---
+++ js/maps.mjs
@@ -0,0 +1,161 @@
+var addListenerHandler;
+// We have to use this to extract the value from the call back I guess
+var placeName;
+// Stores current position
+var POSITION = { 'lat': 62.60, 'lng': 29.76 };
+// The radius, in which we consider the markers to be nearby
+const RADIUS = 100;
+// Dictionary containing id:marker pairs, used to delete markers from map
+var MARKERS = {};
+// Shows the user's position on the map
+var CIRCLE;
+
+/**
+ * Generates random UUID for the database entries.
+ * https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+ */
+var uuidv4 = () => {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+class RemindMap {
+    /**
+     * Initializes the Google Map object.
+     * @param {*} map_id HTML id value of the div, where the map is. 
+     */
+    constructor(map_id) {
+        // Initial location is Joensuu
+        let location = { lat: 62.60, lng: 29.76 };
+        var map = new google.maps.Map(document.getElementById(map_id), {
+            zoom: 12,
+            center: location,
+            disableDefaultUI: true
+        });
+        this.map = map;
+        this.setLocation(true);
+        this.placesService = new google.maps.places.PlacesService(this.map);
+    }
+
+    /**
+     * Using HTML5 geolocation functionality, sets the current position and centers
+     * the map onto it.
+     */
+    setLocation(centerMap = false) {
+        var map = this.map;
+        // Taken from Docs
+        let handleLocationError = () => {
+            console.log('Location error occured.')
+        }
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                POSITION = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                if (centerMap)
+                    map.setCenter(POSITION);
+                CIRCLE.setCenter(POSITION);
+            }, handleLocationError);
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError();
+        }
+    }
+
+    /**
+     * Updates the location of the user and highlights notes if they are nearby.
+     * @param notes List of notes objects.
+     */
+    onUpdateLocation(map, notes) {
+        let count = 0;
+        // Calculates distance between two points
+        let dist = google.maps.geometry.spherical.computeDistanceBetween;
+        this.setLocation();
+        if (notes) {
+            for (let n of notes) {
+                // Check if a note is in the radius and if yes, highlight it
+                if (dist(POSITION, n.location) <= RADIUS) {
+                    $(`#${n.id_}`).addClass('highlighted');
+                    count++;
+                } else {
+                    $(`#${n.id_}`).removeClass('highlighted');
+                }
+            }
+        }
+        // Set the number of nearby markers.
+        $('#nearby-count').html(count);
+    }
+
+    /** Adds a marker to the map.
+     * @param {*} location Location returned by the calling event handler
+     * @param {*} map Map object
+     * @returns Information about the Marker and location
+     */
+    addMarker(location, map, id) {
+        // Add the marker at the clicked location, and add the next-available label
+        // from the array of alphabetical characters.
+        let marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            animation: google.maps.Animation.DROP
+        });
+        google.maps.event.addListener(marker, 'click', () => { focusNote(id) });
+        // Add marker to a dictionary, so it can be removed if necessary
+        MARKERS[id] = marker;
+        // Disable the event listener, if it was activated
+        if (addEventListener)
+            google.maps.event.removeListener(addListenerHandler);
+    }
+
+    /**
+     * When the ADD button is pressed, enables adding markers by clicking
+     * on the map, activating the addListenerHandler.
+     * @param notes List of notes objects.
+     */
+    addNote(notes) {
+        var addMarker = this.addMarker;
+        var placesService = this.placesService;
+        var map = this.map;
+        // Disable the event listener in case it was activated before
+        google.maps.event.removeListener(addListenerHandler);
+        // Changes the add button color to signal, that adding is enabled
+        $('#btn-add').addClass('active');
+        // This event listener calls addMarker() when the map is clicked.
+        addListenerHandler = google.maps.event.addListener(map, 'click',
+            function (event) {
+                let request = {
+                    'radius': 100,
+                    'location': event.latLng,
+                    'fields': ['name', 'formatted_address']
+                }
+                // This method uses Google's Place service to find the nearby places
+                placesService.nearbySearch(request, function (results, status) {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        // Closest place's name
+                        let placeName = results[0]['name'];
+                        let newNote = {
+                            'id_': uuidv4(),
+                            'location': event.latLng,
+                            'locationTitle': placeName,
+                            'text': ""
+                        };
+                        // Add the marker to the MAP
+                        addMarker(event.latLng, map, newNote.id_);
+                        // Push the note to the local registry
+                        notes.push(newNote);
+                        // TODO: Add Push to db
+                        // dbNotePush()
+                        // Redraw the notes using the local registry
+                        redrawNotes(notes);
+                        // The add button is no longer highlighted
+                        $('#btn-add').removeClass('active');
+                        // Change focus to the new note
+                        focusNote(newNote.id_);
+                    }
+                    else {
+                        console.log(status)
+                    }
+                });
+            });
+    }
+}
