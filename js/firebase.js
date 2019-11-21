9b7b4ce4e5a49c4736ba58caf1d01edfe38f7551
Marc <marc.bernalo@e-campus.uab.cat>
2019-11-21 17:39:05 +0200

    commit for marian to check

--- js/firebase.js
+++ js/firebase.js
@@ -2,22 +2,22 @@ function loginGoogle(){
     if(!firebase.auth().currentUser){
         var provider = new firebase.auth.GoogleAuthProvider();
         provider.addScope("https:googleapis.com/auth/plus.login");
-        firebase.auth().signInWithPopup(provider).then (function(result){
-            var token = result.credential.accestoken;
+        console.log("I'm here");
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            console.log("I'm inside popup function")
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
             var user = result.user;
-            console.log(user);
-        }).catch(function (error){
+          }).catch(function(error) {
+            // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
-            var errorMail = error.email;
             var credential = error.credential;
-
             if(errorCode==='auth/account-exists-with-different-credential'){
                 alert('This user does exist');
             }
 
         });
-
     }else{
         firebase.auth().signOut();
     }
